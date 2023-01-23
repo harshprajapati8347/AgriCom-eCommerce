47fb5f9efc0cb56d0d26c2a0ee2621fc05ff7162
harshprajapati8347 <harshprajapati8347@gmail.com>
2023-01-23 22:20:21 +0530

    production

--- server/app.js
+++ server/app.js
@@ -74,13 +74,10 @@ app.use("/api/customize", customizeRouter);
 
 // Production
 
-const __dirname1 = path.resolve();
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/build")));
-
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
