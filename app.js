abb6d95bfd22135f09c7139c1a9c6ef86a7ef4f4
harshprajapati8347 <harshprajapati8347@gmail.com>
2023-02-07 19:54:43 +0530

    build

--- app.js
+++ app.js
@@ -73,11 +73,10 @@ app.use("/api/order", orderRouter);
 app.use("/api/customize", customizeRouter);
 
 // Production
-const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "./client/build")));
+  app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname1, "./client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
