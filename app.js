8e9bffa48b009b07898454c30c77c8f1f4790d11
harshprajapati8347 <harshprajapati8347@gmail.com>
2023-02-14 22:35:01 +0530

    Revert "deploy"
    This reverts commit 87d98bf4fd39fcd2e64035de55c3453e471e245f.

--- app.js
+++ app.js
@@ -74,9 +74,9 @@ app.use("/api/customize", customizeRouter);
 
 // Production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
